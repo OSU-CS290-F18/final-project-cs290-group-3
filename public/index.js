15e5e41e9ddf90180b980e678a2a78b95eaeb611
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-05 12:55:30 -0800

    Update

--- public/index.js
+++ public/index.js
@@ -14,7 +14,15 @@ function insertNewPost(title, imageURL, theme, link) {
 
 }
 
-allPosts = [];
+function getPostIdFromURL() {
+  var path = window.location.pathname;
+  var pathParts = path.split('/');
+  if (pathParts[1] === "posts") {
+    return pathParts[2];
+  } else {
+    return null;
+  }
+}
 
 function handleModalAcceptClick() {
 
@@ -26,17 +34,29 @@ function handleModalAcceptClick() {
   if (!title || !imageURL || !theme || !link) {
     alert("You must fill in all of the fields!");
   } else {
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/posts/' + getPostIdFromURL()  + '/addPost';
+    postRequest.open('POST', requestURL);
 
-    allPosts.push({
+    var requestBody = JSON.stringify({
       title: title,
       imageURL: imageURL,
       theme: theme,
       link: link
     });
 
-    insertNewPost(title, imageURL, theme, link);
-    hidePostSomethingModal();
+    postRequest.addEventListener('load', function (event) {
+      if (event.target.status === 200) {
+        insertNewPost(title, imageURL, theme, link);
+      } else {
+        alert("Error storing post: " + event.target.response);
+      }
+    });
 
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+
+    hidePostSomethingModal();
   }
 
 }
@@ -81,19 +101,6 @@ function hidePostSomethingModal() {
 
 }
 
-/*
- * This function parses an existing DOM element representing a single post
- * into an object representing that post and returns that object.  The object
- * is structured like this:
- *
- * {
- *   description: "...",
- *   photoURL: "...",
- *   price: ...,
- *   city: "...",
- *   condition: "..."
- * }
- */
 function parsePostElem(postElem) {
 
   var post = {
@@ -103,7 +110,7 @@ function parsePostElem(postElem) {
   };
 
   var postImageElem = postElem.querySelector('.post-image-container img');
-  post.photoURL = postImageElem.src;
+  post.imageURL = postImageElem.src;
 
   return post;
 
@@ -114,14 +121,6 @@ function parsePostElem(postElem) {
  * Wait until the DOM content is loaded, and then hook up UI interactions, etc.
  */
 window.addEventListener('DOMContentLoaded', function () {
-
-  /*
-   * Remember all of the initial post elements initially displayed in the page.
-   */
-  var postElems = document.getElementsByClassName('post');
-  for (var i = 0; i < postElems.length; i++) {
-    allPosts.push(parsePostElem(postElems[i]));
-  }
 
   var postSomethingButton = document.getElementById('post-button');
   if (postSomethingButton) {
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-var postData = require('./postData');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUser = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL = 'mongodb://' +
+    mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort +
+    '/' + mongoDBName;
+
+var mongoDB = null;
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -11,34 +24,69 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 function renderIndexPage(req, res, next) {
-  if (postData) {
-    res.status(200).render('postPage', {
-      posts: postData
+    var postsCollection = db.collection('posts');
+    var postCursor = postsCollection.find({});
+    postCursor.toArray(function(err, postDocs) {
+        if (err) {
+            res.status(500).send("Error fetching posts from DB.");
+        } else {
+            res.status(200).render('postPage', {
+                posts: postDocs
+            });
+        }
     });
-  } else {
-    next();
-  }
 };
 
 app.get('/', renderIndexPage);
 app.get('/index.html', renderIndexPage);
 
-app.get('/posts/:n', function (req, res, next) {
-  var n = req.params.n.toLowerCase();
-  if (postData[n]) {
-    res.status(200).render('partials/postCard', postData[n]);
-  } else {
-    next();
-  }
+app.get('/posts/:n', function(req, res, next) {
+    var n = req.params.n.toLowerCase();
+    var postsCollection = db.collection('posts');
+    var postCursor = postsCollection.find({});
+    postCursor.toArray(function(err, postDocs) {
+        if (err) {
+            res.status(500).send("Error fetching posts from DB.");
+        } else {
+            res.status(200).render('partials/postCard', postDocs[n]);
+        }
+    });
 });
 
-app.get('*', function (req, res) {
-  res.status(404).render('404', {});
+app.post('/posts/:post/addPost', function(req, res, next) {
+    var post = req.params.post.toLowerCase();
+    if (req.body && req.body.title && req.body.link && req.body.imageURL && req.body.theme) {
+        var postsCollection = mongoDB.collection('posts');
+        postsCollection.insertOne(
+            {title: req.body.title,
+            imageURL: req.body.imageURL,
+            theme: req.body.theme,
+            link: req.body.link},
+            function(err, result) {
+                if (err) {
+                    res.status(500).send("Error saving post to DB");
+                } else if (!result) {
+                    next();
+                } else {
+                    res.status(200).send("Success");
+                }
+            }
+        );
+    } else {
+        res.status(400).send("Request incomplete");
+    }
 });
 
-app.listen(port, function (err) {
-  if (err) {
-    throw err;
-  }
-  console.log("== Server is listening on port", port);
+app.get('*', function(req, res) {
+    res.status(404).render('404');
+});
+
+MongoClient.connect(mongoURL, function(err, client) {
+    if (err) {
+        throw err;
+    }
+    mongoDB = client.db(mongoDBName);
+    app.listen(port, function() {
+        console.log("== Server listening on port", port);
+    });
 });
